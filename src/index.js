9e816c1edf993eaa10d435cac82d9aa2929ff2a2
Arseni-Bliznets <bliznets04@gmail.com>
2023-07-08 20:49:27 +0300

    feat:end task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arrayOfNumbers = expr.match(/.{1,10}/g)
+    const arraySort = arrayOfNumbers.map(function (o) {
+            const indexOfFirst = o.indexOf(1);
+            const sliceStr = o.slice(indexOfFirst).match(/.{1,2}/g).map(g => {
+                if(g === '10'){
+                    g = '.'
+                } else if (g === '11'){
+                    g = '-'
+                } else {
+                    g ='*'
+                }
+                return g
+            })
+            return sliceStr.join('')
+        })
+        const arrayOfLetters = arraySort.map(k => {
+            if(k === '*'){
+                k = ' '
+            } else {
+            k = MORSE_TABLE[k]
+            }
+            return k
+        })
+        return arrayOfLetters.join('')
 }
 
 module.exports = {
